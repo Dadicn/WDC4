ce8d377a342515a0cba883b0623ffb477895c985
Dadicn <dzoniczbg99@gmail.com>
2021-10-04 11:39:12 +0200

    first-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StagesComponent } from './stages/stages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'stages',
+    component: StagesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
